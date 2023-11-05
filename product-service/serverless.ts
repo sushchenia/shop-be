6ea2781240dafdaeb569811fdaad074011a17b3b
Dzianis Sushchenia <dzianis_sushchenia@epam.com>
2023-11-05 15:32:03 +0100

    task6: implement basic flow

--- product-service/serverless.ts
+++ product-service/serverless.ts
@@ -3,6 +3,7 @@ import type {AWS} from '@serverless/typescript';
 import getProductsById from '@functions/getProductsById'
 import getProductsList from "@functions/getProductsList";
 import createProduct from "@functions/createProduct";
+import catalogBatchProcess from "@functions/catalogBatchProcess";
 
 const serverlessConfiguration: AWS = {
     service: 'product-service',
@@ -10,7 +11,7 @@ const serverlessConfiguration: AWS = {
     plugins: ['serverless-esbuild'],
     provider: {
         name: 'aws',
-        runtime: 'nodejs14.x',
+        runtime: 'nodejs18.x',
         region: "eu-west-1",
         apiGateway: {
             minimumCompressionSize: 1024,
@@ -19,14 +20,49 @@ const serverlessConfiguration: AWS = {
         environment: {
             AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
             NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+            SNS_TOPIC: {
+                Ref: 'createProductTopic',
+            },
         },
         iamRoleStatements: [{
             Effect: 'Allow',
             Action: ['dynamodb:*'],
             Resource: ['arn:aws:dynamodb:eu-west-1:421819764397:table/products', 'arn:aws:dynamodb:eu-west-1:421819764397:table/stocks']
+        }, {
+            Effect: 'Allow',
+            Action: ['sns:*'],
+            Resource: {
+                Ref: 'createProductTopic'
+            }
         }]
     },
-    functions: {getProductsById, getProductsList, createProduct},
+    functions: {getProductsById, getProductsList, createProduct, catalogBatchProcess},
+    resources: {
+        Resources: {
+            catalogItemsQueue: {
+                Type: 'AWS::SQS::Queue',
+                Properties: {
+                    QueueName: 'catalogItemsQueue',
+                },
+            },
+            createProductTopic: {
+                Type: 'AWS::SNS::Topic',
+                Properties: {
+                    TopicName: 'createProductTopic'
+                }
+            },
+            createProductTopicSubscription: {
+                Type: 'AWS::SNS::Subscription',
+                Properties: {
+                    Protocol: 'email',
+                    Endpoint: 'denis.sushchenja@gmail.com',
+                    TopicArn: {
+                        Ref: 'createProductTopic',
+                    },
+                }
+            }
+        }
+    },
     package: {individually: true},
     custom: {
         esbuild: {
@@ -34,7 +70,7 @@ const serverlessConfiguration: AWS = {
             minify: false,
             sourcemap: true,
             exclude: ['aws-sdk'],
-            target: 'node14',
+            target: 'node18',
             define: {'require.resolve': undefined},
             platform: 'node',
             concurrency: 10,

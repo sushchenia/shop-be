f9113ce54904972ce118c4aed241aca52b38803a
Dzianis Sushchenia <dzianis_sushchenia@epam.com>
2023-11-14 02:08:21 +0100

    task7: implement Basic token auth

--- import-service/src/functions/importProductsFile/index.ts
+++ import-service/src/functions/importProductsFile/index.ts
@@ -8,6 +8,13 @@ export default {
                 method: 'get',
                 path: 'import',
                 cors: true,
+                authorizer: {
+                    name: 'basicAuthorizer',
+                    arn: 'arn:aws:lambda:eu-west-1:421819764397:function:authorization-service-dev-basicAuthorizer',
+                    resultTtlInSeconds: 0,
+                    identitySource: 'method.request.header.Authorization',
+                    type: 'token',
+                },
             },
         },
     ],
